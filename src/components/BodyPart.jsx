0109a263ca6118fd365f43bd8b9e8484057d3da5
RoughGhost <stephendlouis6@gmail.com>
2022-06-21 23:57:33 +0100

    first commit

---
+++ src/components/BodyPart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BodyPart = () => {
+  return (
+    <div>
+      <h1>BodyPart</h1>
+    </div>
+  );
+};
+
+export default BodyPart;
---
+++ src/pages/ExceriseDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExceriseDetail = () => {
+  return (
+    <div>
+      <h1>ExceriseDetail</h1>
+    </div>
+  );
+};
+
+export default ExceriseDetail;
